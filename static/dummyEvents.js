be1fc0c96fdaadff0c8aed4b919ba20b6156a6fa
amaygit <mailamaynayak@gmail.com>
2022-03-27 22:38:49 +0530

    nftMarketPlace

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+    {
+      price: 17,
+      from: 'Amay',
+      to: 'Aditya',
+      date: '1 week ago',
+    },
+    {
+      price: 30,
+      from: 'Hemanth',
+      to: 'Aditya',
+      date: '2 days ago',
+    },
+    {
+      price: 24,
+      from: 'Aditya',
+      to: 'Amay',
+      date: '10 days ago',
+    },
+    {
+      price: 30,
+      from: 'Hemanth',
+      to: 'Amay',
+      date: '1 day ago',
+    },
+  ]
